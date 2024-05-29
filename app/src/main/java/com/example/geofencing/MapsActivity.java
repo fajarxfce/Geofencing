221f67ecec801640d7da408a0b60e1f37a67a63f
bayurifkialghifari <bayurifkialgh@gmail.com>
2024-05-29 14:55:38 +0700

    delete unused code

--- app/src/main/java/com/example/geofencing/MapsActivity.java
+++ app/src/main/java/com/example/geofencing/MapsActivity.java
@@ -6,21 +6,16 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
-import android.app.PendingIntent;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingClient;
-import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
@@ -29,12 +24,9 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolygonOptions;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.maps.android.PolyUtil;
 
 import java.util.ArrayList;
@@ -46,7 +38,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private GeofencingClient geofencingClient;
-    private GeofenceHelper geofenceHelper;
 
     private float GEOFENCE_RADIUS = 200;
     private String GEOFENCE_ID = "SOME_GEOFENCE_ID";
@@ -85,7 +76,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         geofencingClient = LocationServices.getGeofencingClient(this);
-        geofenceHelper = new GeofenceHelper(this);
 
         KmlUtil kmlUtil = new KmlUtil();
         polygonPoints = kmlUtil.parseKMLFile(R.raw.contoh, this);
@@ -93,8 +83,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         for (int i = 0; i < polygonPoints.size(); i++) {
             Log.d(TAG, "onCreate: "+polygonPoints.get(i).latitude + ", " + polygonPoints.get(i).longitude);
         }
-
-
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
@@ -128,8 +116,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         enableUserLocation();
         addPolygon(polygonPoints);
-
-//        mMap.setOnMapLongClickListener(this);
     }
 
     private void enableUserLocation() {
@@ -169,37 +155,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-//    @Override
-//    public void onMapLongClick(LatLng latLng) {
-//        if (Build.VERSION.SDK_INT >= 29) {
-//            //We need background permission
-//            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-//                handleMapLongClick(latLng);
-//            } else {
-//                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
-//                    //We show a dialog and ask for permission
-//                    ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_BACKGROUND_LOCATION}, BACKGROUND_LOCATION_ACCESS_REQUEST_CODE);
-//                } else {
-//                    ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_BACKGROUND_LOCATION}, BACKGROUND_LOCATION_ACCESS_REQUEST_CODE);
-//                }
-//            }
-//
-//        } else {
-//            handleMapLongClick(latLng);
-//        }
-//
-//    }
-
     private void handleMapLongClick(LatLng latLng) {
         mMap.clear();
         addMarker(latLng);
-
-
-
-//        KmlUtil kmlUtil = new KmlUtil();
-////        addCircle(latLng, GEOFENCE_RADIUS);
-//        List<LatLng> latLngList = kmlUtil.parseKMLFile(R.raw.contoh, this);
-
 
         List<LatLng> originalPolygonCoords = polygonPoints;
         LatLng originalCenter = getPolygonCenter(originalPolygonCoords);
@@ -213,7 +171,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         addPolygon(adjustedPolygonCoords);
-//        addGeofence(latLng, GEOFENCE_RADIUS);
     }
 
     private LatLng getPolygonCenter(List<LatLng> polygonCoords) {
@@ -226,41 +183,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         return new LatLng(latSum / polygonCoords.size(), lngSum / polygonCoords.size());
     }
 
-//    private void addGeofence(LatLng latLng, float radius) {
-//
-//        Geofence geofence = geofenceHelper.getGeofence(GEOFENCE_ID, latLng, radius, Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_DWELL | Geofence.GEOFENCE_TRANSITION_EXIT);
-//        GeofencingRequest geofencingRequest = geofenceHelper.getGeofencingRequest(geofence);
-//        PendingIntent pendingIntent = geofenceHelper.getPendingIntent();
-//
-//        geofencingClient.addGeofences(geofencingRequest, pendingIntent)
-//                .addOnSuccessListener(new OnSuccessListener<Void>() {
-//                    @Override
-//                    public void onSuccess(Void aVoid) {
-//                        Log.d(TAG, "onSuccess: Geofence Added...");
-//                    }
-//                })
-//                .addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception e) {
-//                        String errorMessage = geofenceHelper.getErrorString(e);
-//                        Log.d(TAG, "onFailure: " + errorMessage);
-//                    }
-//                });
-//    }
-
     private void addMarker(LatLng latLng) {
         MarkerOptions markerOptions = new MarkerOptions().position(latLng);
         mMap.addMarker(markerOptions);
-    }
-
-    private void addCircle(LatLng latLng, float radius) {
-        CircleOptions circleOptions = new CircleOptions();
-        circleOptions.center(latLng);
-        circleOptions.radius(radius);
-        circleOptions.strokeColor(Color.argb(255, 255, 0,0));
-        circleOptions.fillColor(Color.argb(64, 255, 0,0));
-        circleOptions.strokeWidth(4);
-        mMap.addCircle(circleOptions);
     }
 
     private void addPolygon(List<LatLng> latLngList) {
